5188b84dce523989712015ff4ce7d996773077ea
Henrique S. Xavier <hsxavier@if.usp.br>
2019-11-28 15:47:58 -0300

    Created shell script to check error in BigQuery

---
+++ scripts/get_error_message_from_jobID.sh
@@ -0,0 +1,11 @@
+# This script gets a BigQuery jobID as input and return the error messages associated to it.
+# (check the Job ID in the Job Information tab in the Query results in the BG UI).
+
+# Informações sobre como debugar dados do Storage no BigQuery:
+# https://snowplowanalytics.com/blog/2018/12/19/debugging-bad-data-in-gcp-with-bigquery/
+
+jobid=$1
+
+conda activate base
+bq --format=prettyjson show -j $jobid
+conda activate python3
