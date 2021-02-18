d34e998e8e7e3da2c3d59e3b6621301f7641ccab
Henrique S. Xavier <hsxavier@gmail.com>
2021-02-18 11:53:47 -0300

    Consertei checkup de dados duplicados na remuneração de servidores no senado.

--- views/checkup_de_dados.senador_servidores_remuneracao_duplicates.sql
+++ views/checkup_de_dados.senador_servidores_remuneracao_duplicates.sql
@@ -1,8 +1,21 @@
 -- TABELA VERIFICADA: SENADOR_SERVIDORES_REMUNERACAO --
--- Verifica se algum servidor aparece duplicado, provavelmente por uma repetição de chaves
+-- Verifica se algum servidor aparece duplicado, seja por estar duplicado já na 
+-- base de servidores ou por uma repetição de chaves
 -- em algum JOIN. Se aparecerem linhas, isso indica esse problema.
-SELECT setor_exercicio, nome_comissionado, nome_funcao, COUNT(*) AS n
+
+-- Tabela de servidores duplicados na tabela com joins.
+SELECT capture_date, setor_exercicio, nome_comissionado, nome_funcao, COUNT(*) AS n, 'Duplicado na view após JOINs' AS fonte_do_problema
 FROM `gabinete-compartilhado.senado_processed.senador_servidores_remuneracao`
-GROUP BY setor_exercicio, nome_comissionado, nome_funcao
+GROUP BY capture_date, setor_exercicio, nome_comissionado, nome_funcao
 HAVING n > 1
+
+UNION ALL
+
+-- Tabela de servidores duplicados na base fonte (cleaned) de servidores:
+SELECT capture_date, setor_exercicio, nome_comissionado, nome_funcao, COUNT(*) AS n, 'Duplicado já na base de servidores' AS fonte_do_problema
+FROM `gabinete-compartilhado.senado_processed.pessoas_servidores_cleaned`
+WHERE UPPER(setor2) LIKE '%GABSEN%'
+GROUP BY capture_date, setor_exercicio, nome_comissionado, nome_funcao 
+HAVING n > 1
+
 ORDER BY n DESC
