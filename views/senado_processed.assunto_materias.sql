237eb958809b5b00a5d769c2db74f0ab6646cee1
Henrique S. Xavier <hsxavier@gmail.com>
2021-02-18 11:55:13 -0300

    Nova coluna no assunto de matérias do senado: assunto unificado
    Essa coluna seleciona o assunto da proposição principal, se existente;
    e se não existente, seleciona o assunto da proposição associada.

--- views/senado_processed.assunto_materias.sql
+++ views/senado_processed.assunto_materias.sql
@@ -1,6 +1,9 @@
 -- Tabela que, para cada matéria relacionada, lista a matéria raiz:
 WITH p2 AS (
-SELECT rel.IdentificacaoMateria.CodigoMateria as rel_codigo_materia, p.Codigo_Materia, p.Descricao_Subtipo_Materia, p.Numero_Materia, p.Ano_Materia, p.Assunto_Especifico_Descricao
+SELECT 
+  rel.IdentificacaoMateria.CodigoMateria as rel_codigo_materia, 
+  p.Codigo_Materia, p.Descricao_Subtipo_Materia, p.Numero_Materia, p.Ano_Materia, p.Assunto_Especifico_Descricao,
+  p.Ementa_Materia, p.Explicacao_Ementa_Materia
 FROM `gabinete-compartilhado.senado_processed.proposicoes_cleaned` AS p,
 UNNEST(p.Materia_Relacionada) AS rel
 )
@@ -14,7 +17,7 @@ p1.Codigo_Materia AS Codigo_Materia_1,
 p1.Descricao_Subtipo_Materia AS Descricao_Subtipo_Materia_1, 
 p1.Numero_Materia AS Numero_Materia_1, 
 p1.Ano_Materia AS Ano_Materia_1,
-
+p1.Ementa_Materia AS Ementa_Materia_1,
 -- INFORMAÇÕES DO AUTOR DA MATÉRIA
 
 p1.Autoria[OFFSET(0)].SiglaTipoAutor AS Sigla_Tipo_Autor,
@@ -22,7 +25,6 @@ p1.Autoria[OFFSET(0)].IdentificacaoParlamentar.CodigoParlamentar AS Codigo_Parla
 p1.Autoria[OFFSET(0)].IdentificacaoParlamentar.NomeParlamentar AS Nome_Autor, 
 p1.Autoria[OFFSET(0)].IdentificacaoParlamentar.SexoParlamentar AS Sexo,
 p1.Autoria[OFFSET(0)].IdentificacaoParlamentar.FormaTratamento AS Forma_Tratamento,
-
 
 -- Assunto da matéria:
 p1.Assunto_Especifico_Descricao AS Assunto_Especifico_Descricao_1,
@@ -32,9 +34,13 @@ p2.Codigo_Materia AS Codigo_Materia_2,
 p2.Descricao_Subtipo_Materia AS Descricao_Subtipo_Materia_2, 
 p2.Numero_Materia AS Numero_Materia_2, 
 p2.Ano_Materia AS Ano_Materia_2,
+p2.Ementa_Materia AS Ementa_Materia_2,
 
 -- Assunto da matéria raiz:
-p2.Assunto_Especifico_Descricao AS Assunto_Especifico_Descricao_2
+p2.Assunto_Especifico_Descricao AS Assunto_Especifico_Descricao_2,
+
+-- Assunto unificado:
+IF(p1.Assunto_Especifico_Descricao IS NOT NULL, p1.Assunto_Especifico_Descricao, p2.Assunto_Especifico_Descricao) AS Assunto_Especifico_Unificado
 
 FROM `gabinete-compartilhado.senado_processed.proposicoes_cleaned` AS p1
 
