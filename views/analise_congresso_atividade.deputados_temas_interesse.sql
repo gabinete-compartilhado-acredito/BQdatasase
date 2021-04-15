16494f55961e5e318853a707ce2b3555eda6c4d4
Henrique S. Xavier <hsxavier@gmail.com>
2021-04-15 14:50:13 -0300

    Views de interesses dos deputados via proposições
    Criadas views que listam as proposições apresentadas pelos deputados,
    com temas extraídos da própria matéria ou de relacionadas.
    
    E outra view que contabiliza o número de ações.

--- views/analise_congresso_atividade.deputados_temas_interesse.sql
+++ views/analise_congresso_atividade.deputados_temas_interesse.sql
@@ -14,7 +14,8 @@ SELECT
 d.ultima_legislatura, d.id AS id_deputado, d.ultimoStatus_nome, d.sigla_partido, d.uf,
 
 -- Info da proposição de 1a ordem:
-a.idProposicao AS id_prop_1, p1.siglaTipo AS sigla_tipo_1, p1.numero AS numero_1, p1.ano AS ano_1, 
+a.idProposicao AS id_prop_1, p1.siglaTipo AS sigla_tipo_1, p1.numero AS numero_1, p1.ano AS ano_1,
+p1.ementa AS ementa_1,
 -- Tema da proposição de 1a ordem:
 t1.codTema AS cod_tema_1, t1.tema AS tema_1,
 
@@ -26,7 +27,22 @@ t2.codTema AS cod_tema_2, t2.tema AS tema_2,
 -- Info da proposição de 3a ordem:
 p2.id_prop_principal AS id_prop_3, t3.siglaTipo AS sigla_tipo_3, t3.numero AS numero_3, t3.ano AS ano_3, 
 -- Tema da proposição de 3a ordem:
-t3.codTema AS cod_tema_3, t3.tema AS tema_3
+t3.codTema AS cod_tema_3, t3.tema AS tema_3,
+
+-- Código do tema final (o primeiro não nulo na ordem acima):
+CASE
+  WHEN t1.codTema IS NOT NULL THEN t1.codTema
+  WHEN t2.codTema IS NOT NULL THEN t2.codTema
+  WHEN t3.codTema IS NOT NULL THEN t3.codTema
+  ELSE NULL
+END AS cod_tema_final,
+-- Tema final (o primeiro não nulo na ordem acima):
+CASE
+  WHEN t1.tema IS NOT NULL THEN t1.tema
+  WHEN t2.tema IS NOT NULL THEN t2.tema
+  WHEN t3.tema IS NOT NULL THEN t3.tema
+  ELSE NULL
+END AS tema_final
 
 FROM `gabinete-compartilhado.camara_v2_processed.deputados_detalhes_cleaned` AS d
 -- Conecta deputado a proposições (1a ordem) das quais foi autor:
--- views/analise_congresso_atividade.deputados_temas_interesse_consolidado.sql
+++ views/analise_congresso_atividade.deputados_temas_interesse_consolidado.sql
@@ -52,22 +52,33 @@ WITH conta_dep_tema AS (
         WHEN tema_1 IS NOT NULL THEN tema_1
         WHEN tema_2 IS NOT NULL THEN tema_2
         ELSE tema_3
-      END AS tema
-
+      END AS tema,
+      -- Contagem de proposições apresentadas: 
+      COUNT(DISTINCT id_prop_1) OVER (PARTITION BY id_deputado) AS n_proposicoes, 
+    
     FROM `gabinete-compartilhado.analise_congresso_atividade.deputados_temas_interesse`
   )
 
   -- Tabela que conta, para cada parlamentar, o número de intervenções (proposições) associadas a cada tema:
   SELECT 
-    pt.id_deputado, pt.ultimoStatus_nome, pt.sigla_partido, pt.uf, pt.tema, COUNT(DISTINCT pt.id_proposicao_tema) AS n_intervencoes, n_anos_atuacao
+    pt.id_deputado, 
+    ANY_VALUE(pt.ultimoStatus_nome) AS ultimoStatus_nome, 
+    ANY_VALUE(pt.sigla_partido) AS sigla_partido, 
+    ANY_VALUE(pt.uf) AS uf, 
+    pt.tema,
+    -- Número de intervenções no tema:
+    COUNT(DISTINCT pt.id_proposicao_autoria) AS n_intervencoes,
+    ANY_VALUE(n_proposicoes) AS n_intervencoes_total,
+    -- Número de anos nos quais o deputado apresentou ao menos uma proposição:
+    ANY_VALUE(n_anos_atuacao) AS n_anos_atuacao
   FROM prop_tema AS pt
   WHERE pt.ultima_legislatura = 56
-  GROUP BY pt.id_deputado, pt.ultimoStatus_nome, pt.sigla_partido, pt.uf, pt.tema, pt.n_anos_atuacao
+  GROUP BY pt.id_deputado, pt.tema
 )
 
 -- Adiciona à Tabela acima uma coluna com o número total de intervenções de cada deputado:
 SELECT 
-  id_deputado, ultimoStatus_nome, sigla_partido, uf, tema, n_intervencoes, 
+  id_deputado, ultimoStatus_nome, sigla_partido, uf, tema, n_intervencoes, n_intervencoes_total, 
   SUM(n_intervencoes) OVER (PARTITION BY id_deputado) AS n_total, n_anos_atuacao
 FROM conta_dep_tema 
 ORDER BY ultimoStatus_nome, n_intervencoes DESC
