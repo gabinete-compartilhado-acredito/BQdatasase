5e3a6b1664d67f64a266df6d6ebc1f7717e82a09
Henrique S. Xavier <hsxavier@gmail.com>
2022-05-31 15:20:03 -0300

    Desligados scheduled queries do dashboard do twitter do Cristiano e dos painéis de alinhamento do congresso

--- scheduled_queries/apoio_deputados_governo.sql
+++
@@ -1,3 +0,0 @@
-# destination_table: datastudio.apoio_deputados_governo_
-
-SELECT * FROM `gabinete-compartilhado.paineis.apoio_deputados_governo`
--- scheduled_queries/projeto twitter.sql
+++
@@ -1,5 +0,0 @@
-# destination_table: redes_sociais.projeto_twitter
-
-SELECT 'Deputado Federal' as titulo, * FROM `gabinete-compartilhado.redes_sociais.deputados_tweets_cleaned` 
-union all
-SELECT 'Senador' as titulo, * FROM `gabinete-compartilhado.redes_sociais.senadores_tweets_cleaned`
